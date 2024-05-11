09edaa8c983577ed28977919f7db8f4558b9d707
Mistium <92952823+Mistium@users.noreply.github.com>
2024-05-11 18:21:03 +0100

    Update OASM.js

--- featured/OASM.js
+++ featured/OASM.js
@@ -2,23 +2,22 @@
 // By: @mistium on discord
 // Description: Run the full oasm interpreter except very fast.
 
-(function (Scratch) {
+(function(Scratch) {
   "use strict";
 
   const vm = Scratch.vm,
-  runtime = vm.runtime;
-  
+    runtime = vm.runtime;
+
   if (!Scratch.vm.extensionManager.isExtensionLoaded("pen")) {
     runtime.extensionManager.loadExtensionIdSync("pen");
   }
-  
+
   class OASM {
     getInfo() {
       return {
         id: 'OASM',
         name: 'OASM',
-        blocks: [
-          {
+        blocks: [{
             func: 'docs',
             blockType: Scratch.BlockType.BUTTON,
             text: 'Learn OASM',
@@ -28,10 +27,10 @@
             blockType: Scratch.BlockType.REPORTER,
             text: 'Compile [CODE]',
             arguments: {
-                CODE: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue: '["setv msg hello","prnt msg"]'
-                },
+              CODE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '["setv msg hello","prnt msg"]'
+              },
             },
           },
           {
@@ -40,18 +39,18 @@
             blockType: Scratch.BlockType.COMMAND,
             text: 'Run  At [X],[Y] Code: [CODE]',
             arguments: {
-                CODE: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue: ''
-                },
-                X: {
-                  type: Scratch.ArgumentType.NUMBER,
-                  defaultValue: 0
-                },
-                Y: {
-                  type: Scratch.ArgumentType.NUMBER,
-                  defaultValue: 0
-                },
+              CODE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: ''
+              },
+              X: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: 0
+              },
+              Y: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: 0
+              },
             },
           },
           {
@@ -59,18 +58,18 @@
             blockType: Scratch.BlockType.REPORTER,
             text: 'Run  At [X],[Y] Code: [CODE]',
             arguments: {
-                CODE: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue: ''
-                },
-                X: {
-                  type: Scratch.ArgumentType.NUMBER,
-                  defaultValue: 0
-                },
-                Y: {
-                  type: Scratch.ArgumentType.NUMBER,
-                  defaultValue: 0
-                },
+              CODE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: ''
+              },
+              X: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: 0
+              },
+              Y: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: 0
+              },
             },
           },
           {
@@ -94,21 +93,21 @@
     run({CODE,X,Y}) {
       CODE = JSON.parse(CODE)
       const target = vm.editingTarget
-      target.setXY(X,Y);
+      target.setXY(X, Y);
       this.vars = []
       this.pc = 1
       this.output = []
       const comp = CODE.length / 4 + 1
       while (this.pc < comp) {
-        const temp = this.pc * 4 - 1
-        this.cmd = CODE[temp - 3]
-        this.in1 = CODE[temp - 2]-1
+        const temp = (this.pc * 4) - 1
+        const cmd = CODE[temp - 3]
+        this.in1 = CODE[temp - 2] - 1
         this.in2 = CODE[temp - 1]
         this.in3 = CODE[temp]
-        switch (this.cmd) {
+        switch (cmd) {
           case "1":
-            this.vars[this.in1] = ""    
-          break;
+            this.vars[this.in1] = ""
+            break;
           case "2":
             if (isNaN(this.in2)) {
               this.vars[this.in1] = this.in2
@@ -116,132 +115,132 @@
               this.vars[this.in1] = parseInt(this.in2)
             }
             break;
-            case "3":
-              this.vars[this.in1] += this.vars[this.in2-1]
+          case "3":
+            this.vars[this.in1] += this.vars[this.in2 - 1]
             break;
-            case "4":
-              this.pc = +this.in1+1
+          case "4":
+            this.pc = +this.in1 + 1
             break;
-            case "5":
-              if (this.vars[this.in1] === this.vars[this.in2-1]) {
-                this.pc = +this.in3
-              }
+          case "5":
+            if (this.vars[this.in1] === this.vars[this.in2 - 1]) {
+              this.pc = +this.in3
+            }
             break;
-            case "6":
-              if (this.vars[this.in1] > this.vars[this.in2-1]) {
-                this.pc = +this.in3
-              }
+          case "6":
+            if (this.vars[this.in1] > this.vars[this.in2 - 1]) {
+              this.pc = +this.in3
+            }
             break;
-            case "7":
-              if (this.vars[this.in1] < this.vars[this.in2-1]) {
-                this.pc = +this.in3
-              }
+          case "7":
+            if (this.vars[this.in1] < this.vars[this.in2 - 1]) {
+              this.pc = +this.in3
+            }
             break;
-            case "8":
-              this.output.push(this.vars[this.in1])
+          case "8":
+            this.output.push(this.vars[this.in1])
             break;
-            case "9":
-              if (this.vars[this.in1] <= this.vars[this.in2-1]) {
-                this.pc = +this.in3
-              }
+          case "9":
+            if (this.vars[this.in1] <= this.vars[this.in2 - 1]) {
+              this.pc = +this.in3
+            }
             break;
-            case "10":
-              if (this.vars[this.in1] >= this.vars[this.in2-1]) {
-                this.pc = +this.in3
-              }
+          case "10":
+            if (this.vars[this.in1] >= this.vars[this.in2 - 1]) {
+              this.pc = +this.in3
+            }
             break;
-            case "11":
-              this.vars[this.in1] = +this.vars[this.in2-1]
+          case "11":
+            this.vars[this.in1] = +this.vars[this.in2 - 1]
             break;
-            case "12":
-              this.vars[this.in1] *= this.vars[this.in2-1]
+          case "12":
+            this.vars[this.in1] *= this.vars[this.in2 - 1]
             break;
-            case "13":
-              this.vars[this.in1] /= this.vars[this.in2-1]
+          case "13":
+            this.vars[this.in1] /= this.vars[this.in2 - 1]
             break;
-            case "14":
-              this.vars[this.in1] -= this.vars[this.in2-1]
+          case "14":
+            this.vars[this.in1] -= this.vars[this.in2 - 1]
             break;
-            case "15":
-              runtime.ext_pen._penDown(target);
+          case "15":
+            runtime.ext_pen._penDown(target);
             break;
-            case "16":
-              runtime.ext_pen._penUp(target);
+          case "16":
+            runtime.ext_pen._penUp(target);
             break;
-            case "17":
-              runtime.ext_pen._setPenColorToColor(this.vars[this.in1], target);
+          case "17":
+            runtime.ext_pen._setPenColorToColor(this.vars[this.in1], target);
             break;
-            case "18":
-              runtime.ext_pen._setPenSizeTo(this.vars[this.in1], target);
+          case "18":
+            runtime.ext_pen._setPenSizeTo(this.vars[this.in1], target);
             break;
-            case "19":
-              runtime.ext_pen.clear();
+          case "19":
+            runtime.ext_pen.clear();
             break;
-            case "20":
-              target.setXY(X + this.vars[this.in1],target.y);
+          case "20":
+            target.setXY(X + this.vars[this.in1], target.y);
             break;
-            case "21":
-              target.setXY(target.x,Y + this.vars[this.in1]);
+          case "21":
+            target.setXY(target.x, Y + this.vars[this.in1]);
             break;
-            case "22":
-              target.setXY(X + this.vars[this.in1],Y + this.vars[this.in2-1]);
+          case "22":
+            target.setXY(X + this.vars[this.in1], Y + this.vars[this.in2 - 1]);
             break;
-            case "23":
+          case "23":
             break;
-            case "24":
-              this.vars[this.in2-1] = 0
-              this.in1 = CODE[temp - 2]
-              if (this.in1 === "mousepos") {
-                this.vars[this.in2-1] = runtime.ioDevices.mouse.getScratchX() - X
-                this.vars[this.in3-1] = runtime.ioDevices.mouse.getScratchY() - Y
-              } else if (this.in1 === "timestamp") {
-                this.vars[this.in2-1] = Date.now()
-              } else if (this.in1 === "mouseclick") {
-                this.vars[this.in2-1] = ((+runtime.ioDevices.mouse.getIsDown() || 0) - 0)
-              } else if (this.in1 === "timer") {
-                this.vars[this.in2-1] = runtime.ioDevices.clock.projectTimer()
-              } else if (this.in2 === "line") {
-                this.vars[this.in2-1] = this.pc
-              } else if (this.in1 === ("key" + runtime.ioDevices.keyboard.getLastKeyPressed())) {
-                this.vars[this.in2-1] = ((+runtime.ioDevices.keyboard.getKeyIsDown(runtime.ioDevices.keyboard.getLastKeyPressed())))
-              }
+          case "24":
+            this.vars[this.in2 - 1] = 0
+            this.in1 = CODE[temp - 2]
+            if (this.in1 === "mousepos") {
+              this.vars[this.in2 - 1] = runtime.ioDevices.mouse.getScratchX() - X
+              this.vars[this.in3 - 1] = runtime.ioDevices.mouse.getScratchY() - Y
+            } else if (this.in1 === "timestamp") {
+              this.vars[this.in2 - 1] = Date.now()
+            } else if (this.in1 === "mouseclick") {
+              this.vars[this.in2 - 1] = ((+runtime.ioDevices.mouse.getIsDown() || 0) - 0)
+            } else if (this.in1 === "timer") {
+              this.vars[this.in2 - 1] = runtime.ioDevices.clock.projectTimer()
+            } else if (this.in2 === "line") {
+              this.vars[this.in2 - 1] = this.pc
+            } else if (this.in1 === ("key" + runtime.ioDevices.keyboard.getLastKeyPressed())) {
+              this.vars[this.in2 - 1] = ((+runtime.ioDevices.keyboard.getKeyIsDown(runtime.ioDevices.keyboard.getLastKeyPressed())))
+            }
             break;
-            case "25":
-              this.vars[this.in1] = Math.sin(this.vars[this.in1]);
+          case "25":
+            this.vars[this.in1] = Math.sin(this.vars[this.in1]);
             break;
-            case "26":
-              this.vars[this.in1] = Math.cos(this.vars[this.in1]);
+          case "26":
+            this.vars[this.in1] = Math.cos(this.vars[this.in1]);
             break;
-            case "27":
-              this.vars[this.in1] = Math.tan(this.vars[this.in1]);
+          case "27":
+            this.vars[this.in1] = Math.tan(this.vars[this.in1]);
             break;
-            case "28":
-              this.vars[this.in1] %= this.vars[this.in2-1];
+          case "28":
+            this.vars[this.in1] %= this.vars[this.in2 - 1];
             break;
-            case "29":
-              this.vars[this.in1] = Math.sqrt(this.vars[this.in1]);
+          case "29":
+            this.vars[this.in1] = Math.sqrt(this.vars[this.in1]);
             break;
-            case "30":
-              this.vars[this.in1] = this.vars[this.vars[this.in2-1]-1];
+          case "30":
+            this.vars[this.in1] = this.vars[this.vars[this.in2 - 1] - 1];
             break;
-            case "31":
-              this.vars[this.in3-1] = this.vars[this.in1][this.vars[this.in2-1]-1];
+          case "31":
+            this.vars[this.in3 - 1] = this.vars[this.in1][this.vars[this.in2 - 1] - 1];
             break;
-            case "32":
-              this.vars[this.in2-1] = this.vars[this.in1].length;
+          case "32":
+            this.vars[this.in2 - 1] = this.vars[this.in1].length;
             break;
-            case "33":
-              this.vars[this.in3-1] = this.vars[this.in1] + this.vars[this.in2-1]
+          case "33":
+            this.vars[this.in3 - 1] = this.vars[this.in1] + this.vars[this.in2 - 1]
             break;
-            default:
-              console.log("Unknown Command: " + this.cmd);
-          }
-          this.pc++;
+          default:
+            console.log("Unknown Command: " + cmd);
         }
-        return this.output.length > 0 ? JSON.stringify(this.output) : '[]';
+        this.pc+=1;
       }
+      return this.output.length > 0 ? JSON.stringify(this.output) : '[]';
+    }
 
-    
+
     lastvars() {
       return JSON.stringify(this.vars)
     }
@@ -249,17 +248,20 @@
     lastoutput() {
       return JSON.stringify(this.output)
     }
-    
+
+    allcmds() {
+      return ["totv", "setv", "chav", "jump", "equl", "gthn", "lthn", "prnt", "ngth", "nlth", "svto", "mulv", "divv", "subv", "pend", "penu", "penc", "pens", "pene", "setx", "sety", "setp", "labl", "getd", "sinv", "cosv", "tanv", "modv", "sqrt", "copy", "letr", "leng"]
+    }
+
     compile({CODE}) {
-      const all_oasm_commands = ["totv","setv","chav","jump","equl","gthn","lthn","prnt","ngth","nlth","svto","mulv","divv","subv","pend","penu","penc","pens","pene","setx","sety","setp","labl","getd","sinv","cosv","tanv","modv","sqrt","copy","letr","leng"]
-      const all_oasm_jumps = ["jump","equl","gthn","lthn","ngth","nlth"]
+      const all_oasm_commands = ["totv", "setv", "chav", "jump", "equl", "gthn", "lthn", "prnt", "ngth", "nlth", "svto", "mulv", "divv", "subv", "pend", "penu", "penc", "pens", "pene", "setx", "sety", "setp", "labl", "getd", "sinv", "cosv", "tanv", "modv", "sqrt", "copy", "letr", "leng"]
+      const all_oasm_jumps = ["jump", "equl", "gthn", "lthn", "ngth", "nlth"]
       CODE = JSON.parse(CODE)
       this.vars = []
       this.commands = []
       this.item = ""
-      for (this.i in CODE) {
-        this.item = CODE[this.i]
-        this.cur = this.item.split(" ")
+      for (let i = 0; i < CODE.length; i++) {
+        this.cur = CODE[i].split(" ")
         this.cur = this.cur.concat(Array(4 - this.cur.length).fill("0"))
         if (this.cur[0] === "labl") {
           this.mapcur = []
@@ -267,7 +269,7 @@
             this.mapcur = x.split(" ")
             if (all_oasm_jumps.indexOf(this.mapcur[0]) !== -1) {
               if (this.mapcur[3] === this.cur[1]) {
-                this.mapcur[3] = this.i.toString()
+                this.mapcur[3] = i.toString()
               } else if (this.mapcur[1] === this.cur[1]) {
                 this.mapcur[1] = this.i.toString()
               }
@@ -285,9 +287,9 @@
               if (this.mapcur[1] === this.cur[1]) {
                 this.mapcur[1] = this.len
               } else if (this.mapcur[2] === this.cur[1]) {
-                  this.mapcur[2] = this.len.toString()
+                this.mapcur[2] = this.len.toString()
               } else if (this.mapcur[3] === this.cur[1]) {
-                  this.mapcur[3] = this.len.toString()
+                this.mapcur[3] = this.len.toString()
               }
               return this.mapcur.join(" ")
             })
@@ -298,8 +300,7 @@
         this.commands = this.commands.concat(this.cur)
       }
       if (this.vars.length > 0) {
-        this.item = ["1","0","",""]
-        this.item[1] = this.vars.length.toString()
+        this.item = ["1", this.vars.length.toString(), "", ""] // totv
         this.commands = this.item.concat(this.commands)
       }
       return JSON.stringify(this.commands);
