aaaa742129ddd810c65c88e2140244efef2a80db
Mistium <92952823+Mistium@users.noreply.github.com>
2024-05-19 16:13:21 +0100

    Update and rename tempVars.js to TempVars.js

--- files/TempVars.js
+++ files/TempVars.js
@@ -1,33 +1,19 @@
-// remaking this for turbowarp
-
-/**
- * Class
- * @constructor
- */
-
 (function(Scratch) {
   "use strict";
 
-class tempVars {
-    constructor (runtime) {
-        /**
-         * The runtime instantiating this block package.
-         * @type {runtime}
-         */
+class TempVars {
+    constructor(runtime) {
         this.runtime = runtime;
     }
 
-    getThreadVars (thread) {
+    getThreadVars(thread) {
         if (!thread.tempVars) {
             thread.tempVars = Object.create(null);
         }
         return thread.tempVars;
     }
 
-    /**
-     * @returns {object} metadata for this extension and its blocks.
-     */
-    getInfo () {
+    getInfo() {
         return {
             id: 'tempVars',
             name: 'Temporary Variables',
@@ -60,7 +46,7 @@ class tempVars {
                         },
                         value: {
                             type: Scratch.ArgumentType.NUMBER,
-                            defaultValue: '1'
+                            defaultValue: 1
                         }
                     },
                     blockType: Scratch.BlockType.COMMAND
@@ -74,7 +60,6 @@ class tempVars {
                             defaultValue: 'Variable'
                         }
                     },
-                    allowDropAnywhere: true,
                     blockType: Scratch.BlockType.REPORTER
                 },
                 '---',
@@ -103,19 +88,11 @@ class tempVars {
                             defaultValue: 'Variable'
                         }
                     },
-                    disableMonitor: true,
                     blockType: Scratch.BlockType.BOOLEAN
                 },
                 {
                     opcode: 'allVariables',
                     text: 'current variables',
-                    arguments: {
-                        name: {
-                            type: Scratch.ArgumentType.STRING,
-                            defaultValue: 'Variable'
-                        }
-                    },
-                    disableMonitor: true,
                     blockType: Scratch.BlockType.REPORTER
                 },
                 '---',
@@ -138,55 +115,14 @@ class tempVars {
             ]
         };
     }
-    /**
-     * This function is used for any compiled blocks in the extension if they exist.
-     * Data in this function is given to the IR & JS generators.
-     * Data must be valid otherwise errors may occur.
-     * @returns {object} functions that create data for compiled blocks.
-     * @deprecated nolonger in use as all of this is now inside the compiler
-     */
-    getCompileInfoOld() {
-        return {
-            ir: {
-                forEachTempVar: (generator, block) => {
-                    generator.analyzeLoop();
-                    return {
-                        kind: 'stack',
-                        name: generator.descendInputOfBlock(block, 'NAME'),
-                        repeat: generator.descendInputOfBlock(block, 'REPEAT'),
-                        do: generator.descendSubstack(block, 'SUBSTACK')
-                    };
-                }
-            },
-            js: {
-                forEachTempVar: (node, compiler, imports) => {
-                    const index = compiler.localVariables.next();
-                    const name = compiler.localVariables.next();
-                    const inputName = compiler.descendInput(node.name).asString();
-                    const realName = `('threadVar_' + ${inputName})`;
-                    compiler.source += `var ${index} = 0; `;
-                    compiler.source += `var ${name} = ${realName}; `;
-                    compiler.source += `while (${index} < ${compiler.descendInput(node.repeat).asNumber()}) { `;
-                    compiler.source += `${index}++; `;
-                    compiler.source += `if (!thread.tempVars) { `;
-                    compiler.source += `thread.tempVars = {}; `;
-                    compiler.source += `}\n`;
-                    compiler.source += `thread.tempVars[${name}] = ${index};\n`;
-                    compiler.descendStack(node.do, new imports.Frame(true));
-                    compiler.yieldLoop();
-                    compiler.source += '}\n';
-                }
-            }
-        }
-    }
 
-    setVariable({name,value}, util) {
+    setVariable({name, value}, util) {
         const tempVars = this.getThreadVars(util.thread);
         name = `threadVar_${name}`;
         tempVars[name] = value;
     }
 
-    changeVariable({name,value}, util) {
+    changeVariable({name, value}, util) {
         const tempVars = this.getThreadVars(util.thread);
         name = `threadVar_${name}`;
         const oldNum = Number(tempVars[name]);
@@ -202,7 +138,7 @@ class tempVars {
         const tempVars = this.getThreadVars(util.thread);
         name = `threadVar_${name}`;
         const value = tempVars[name];
-        if (!value) return '';
+        if (value === undefined) return '';
         return value;
     }
 
@@ -212,46 +148,41 @@ class tempVars {
         if (!(name in tempVars)) return;
         delete tempVars[name];
     }
-    
-    deleteAllVariables({_}, util) {
-        // resets the vars
+
+    deleteAllVariables(_, util) {
         util.thread.tempVars = Object.create(null);
     }
-    
-    variableExists ({args}, util) {
+
+    variableExists({name}, util) {
         const tempVars = this.getThreadVars(util.thread);
-        const name = `threadVar_${args.name}`;
-        return (name in tempVars);
+        const varName = `threadVar_${name}`;
+        return (varName in tempVars);
     }
 
-    allVariables ({_}, util) {
+    allVariables(_, util) {
         const tempVars = this.getThreadVars(util.thread);
         const keys = Object.keys(tempVars);
         const mapped = keys.map(name => name.replace('threadVar_', ''));
         return JSON.stringify(mapped);
     }
 
-    forEachTempVar ({args}, util) {
-        // compiled blocks need an interpreter version
-        // for edge activated hats
-        const count = Cast.toNumber(args.REPEAT);
-        const name = Cast.toString(args.NAME);
-        // Initialize loop
+    forEachTempVar({NAME, REPEAT}, util) {
+        const count = Cast.toNumber(REPEAT);
+        const name = Cast.toString(NAME);
+
         if (typeof util.stackFrame.loopCounter === 'undefined') {
             util.stackFrame.loopCounter = count;
         }
-        // Only execute once per frame.
-        // When the branch finishes, `repeat` will be executed again and
-        // the second branch will be taken, yielding for the rest of the frame.
-        // Decrease counter
+
         util.stackFrame.loopCounter--;
-        // If we still have some left, start the branch.
+
         if (util.stackFrame.loopCounter >= 0) {
-            const i = (count - (util.stackFrame.loopCounter)) - 1;
+            const i = (count - util.stackFrame.loopCounter) - 1;
             this.setVariable({ name: name, value: i + 1 }, util);
             util.startBranch(1, true);
         }
     }
 }
-  Scratch.extensions.register(new tempVars());
+
+Scratch.extensions.register(new TempVars());
 })(Scratch);
