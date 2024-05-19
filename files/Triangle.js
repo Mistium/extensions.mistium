2db8a738dc788f5d9e4d16b245dbc4522ca0fdfc
Mistium <92952823+Mistium@users.noreply.github.com>
2024-05-19 10:54:22 +0100

    Update and rename triangle.js to Triangle.js

--- files/Triangle.js
+++ files/Triangle.js
@@ -2,10 +2,8 @@
 // This Source Code is subject to the terms of the Mozilla Public License, v2.0,
 // If a copy of the MPL was not distributed with this file,
 // Then you can obtain one at https://mozilla.org/MPL/2.0/
+
 (function(Scratch) {
-    if (!Scratch.extensions.unsandboxed) {
-      throw new Error("EvalPlus must be unsandboxed");
-    }
 
     const vm = Scratch.vm,
     runtime = vm.runtime;
