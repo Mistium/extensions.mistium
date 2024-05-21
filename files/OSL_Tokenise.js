fcffa13a350c97f28e708c6bf87ea8006fb6c091
Mistium <92952823+Mistium@users.noreply.github.com>
2024-05-21 14:23:13 +0100

    Update OSL_Tokenise.js

--- files/OSL_Tokenise.js
+++ files/OSL_Tokenise.js
@@ -1,4 +1,4 @@
-(function (Scratch) {
+(function(Scratch) {
   "use strict";
 
   class OSLTokenise {
@@ -6,21 +6,20 @@
     constructor() {
       this.regex = /"[^"\\]*(?:\\.[^"\\]*)*"|{[^}]+}|\[[^\]]+\]|\b\d[\d.]*\b|\S+/g;
     }
-    
+
     getInfo() {
       return {
         id: 'OSLTokenise',
         name: 'OSL Tokenise',
-        blocks: [
-          {
+        blocks: [{
             opcode: 'tokenise',
             blockType: Scratch.BlockType.REPORTER,
             text: 'Tokenise OSL [CODE]',
             arguments: {
-                CODE: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue: "log \"hello\""
-                },
+              CODE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "log \"hello\""
+              },
             },
           },
           {
@@ -28,21 +27,78 @@
             blockType: Scratch.BlockType.REPORTER,
             text: 'Tokenise Methods [CODE]',
             arguments: {
-                CODE: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue: "\"hello\".index(\"l\").bool"
-                },
+              CODE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "\"hello\".index(\"l\").bool"
+              },
+            },
+          },
+          {
+            opcode: 'getMethodInputs',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Get Method Inputs [CODE]',
+            arguments: {
+              CODE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'hi("wow","test")'
+              },
             },
           },
         ],
       };
     }
 
-    splitmethods({CODE}) {
+    splitmethods({
+      CODE
+    }) {
       return JSON.stringify(CODE.match(this.regex) || []);
     }
 
-    tokenise({CODE}) {
+    getMethodInputs({
+      CODE
+    }) {
+      const methodName = CODE.split('(')[0].trim();
+      const argsString = CODE.match(/\(([^)]+)\)/)[1];
+      const args = [];
+      let currentArg = '';
+      let inQuotes = false;
+
+      for (let i = 0; i < argsString.length; i++) {
+        const char = argsString.charAt(i);
+        if (char === ',' && !inQuotes) {
+          args.push(currentArg.trim());
+          currentArg = '';
+        } else {
+          currentArg += char;
+          if (char === '"') inQuotes = !inQuotes;
+        }
+      }
+      if (currentArg.trim() !== '') {
+        args.push(currentArg.trim());
+      }
+
+      let mapargs = args.map(arg => {
+        arg = arg.trim();
+        if (arg.startsWith('"') && arg.endsWith('"')) {
+          return arg;
+        } else if (!isNaN(arg)) {
+          return Number(arg);
+        } else if (arg.startsWith('[') && arg.endsWith(']')) {
+          return JSON.parse(arg);
+        } else {
+          return arg;
+        }
+      });
+      if (typeof mapargs == "object") {
+        return JSON.stringify(mapargs)
+      } else {
+        return mapargs
+      }
+    }
+
+    tokenise({
+      CODE
+    }) {
       let tokens = [];
       let inQuotes = false;
       let currentToken = '';
@@ -52,8 +108,6 @@
         if (char === '"') {
           inQuotes = !inQuotes;
           currentToken += char;
-        } else if (inQuotes && char === '\\') {
-          currentToken += char + CODE[++i];
         } else if (!inQuotes && char === ' ') {
           if (currentToken.trim() !== '') {
             tokens.push(currentToken);
