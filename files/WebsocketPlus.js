b52860b890230b5340480455934bea45c3ac8346
Mistium <92952823+Mistium@users.noreply.github.com>
2024-05-28 20:43:59 +0100

    Update WebsocketPlus.js

--- files/WebsocketPlus.js
+++ files/WebsocketPlus.js
@@ -1,3 +1,7 @@
+// Made by @mistium on discord,
+// this extension is for originOS :P
+// Thanks for using my extension :D
+
 // License: MPL-2.0
 // This Source Code is subject to the terms of the Mozilla Public License, v2.0,
 // If a copy of the MPL was not distributed with this file,
