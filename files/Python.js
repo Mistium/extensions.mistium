1119fb9e13df0924f4f8e99b48a0283d6fa9e8e1
Mistium <92952823+Mistium@users.noreply.github.com>
2024-07-05 01:48:54 +0100

    Update Python.js

--- files/Python.js
+++ files/Python.js
@@ -1,116 +1,184 @@
-// Name: Python
-// I made the best scratch extension :D
-// License: MPL-2.0
-// This Source Code is subject to the terms of the Mozilla Public License, v2.0,
-// If a copy of the MPL was not distributed with this file,
-// Then you can obtain one at https://mozilla.org/MPL/2.0/
+// ID: Python
+// Author: Mistium
 
 (function (Scratch) {
-    if (!Scratch.extensions.unsandboxed) {
-        throw new Error("Python must be unsandboxed");
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error("Python must be unsandboxed");
+  }
+
+  async function setupPyodide() {
+    if (typeof pyodide === 'undefined') {
+      const languagePluginLoader = import('https://cdn.jsdelivr.net/pyodide/v0.18.1/full/pyodide.js');
+      const pyodideUrl = 'https://cdn.jsdelivr.net/pyodide/v0.18.1/full/';
+      await languagePluginLoader;
+      pyodide = await loadPyodide({ indexURL: pyodideUrl });
     }
-    async function setupPyodide() {
-        if (navigator.onLine) {
-            try {
-                const response = await Scratch.fetch("https://corsproxy.io/?url=https://cdn.jsdelivr.net/pyodide/v0.25.1/full/pyodide.js");
-                const scriptText = await response.text();
-                const scriptElement = document.createElement(('script'));
-                scriptElement.textContent = scriptText;
-                document.body.appendChild(scriptElement);
-                scriptElement.id = "EvalPlus_pyodide";
-                // Assuming pyodide.js defines pyodide object globally
-                // You can check if pyodide object is available
-            } catch (e) {
-                alert(e);
-            }
-        } else {
-            alert("You must be online to import Python.");
-        }
-    }
+  }
 
-
-    class Python {
-        constructor() {
-            this.enabled = true; // Eval is enabled by default
-            this.pythonLoaded = false;
-            setupPyodide()
-        }
-
-        getInfo() {
-            return {
-                id: 'MistiumPython',
-                name: 'Python',
-                color1: '#b58707',
-                blocks: [
-                    {
-                        opcode: 'runPyAsync',
-                        blockType: Scratch.BlockType.REPORTER,
-                        text: 'Python Async [CODE]',
-                        arguments: {
-                            CODE: { type: Scratch.ArgumentType.STRING, defaultValue: '' }
-                        },
-                    },
-                    {
-                        opcode: 'runPy',
-                        blockType: Scratch.BlockType.REPORTER,
-                        text: 'Python Eval [CODE]',
-                        arguments: {
-                            CODE: { type: Scratch.ArgumentType.STRING, defaultValue: '' }
-                        },
-                    },
-                    {
-                        opcode: 'getvar',
-                        blockType: Scratch.BlockType.REPORTER,
-                        text: 'Get Variable [NAME]',
-                        arguments: {
-                            NAME: { type: Scratch.ArgumentType.STRING, defaultValue: '' }
-                        },
-                    },
-                    {
-                        opcode: 'resetvars',
-                        blockType: Scratch.BlockType.COMMAND,
-                        text: 'Reset Variables',
-                    }
-                ]
-            };
-        }
-
-        runPyAsync({ CODE }) {
-            try {
-                return pyodide.runPythonAsync(CODE);
-            } catch (error) {
-                console.error("Error:", error);
-            }
-        }
-
-        runPy({ CODE }) {
-            try {
-                return pyodide.runPython(CODE);
-            } catch (error) {
-                console.error("Error:", error);
-            }
-        }
-
-      resetvars() {
-            try {
-                pyodide.globals = {};
-            } catch (error) {
-                console.error("Error:", error);
-            }
-        }
-      
-      getvar({ NAME }) {
-        try {
-          if (typeof pyodide === 'undefined') {
-            throw new Error("Pyodide object not found.");
-          }
-          return pyodide.globals[NAME];
-        } catch (error) {
-          console.error("Error:", error);
-          return null;
-        }
+  class Python {
+    constructor() {
+      alert("Python Will Not Work When Offline! It will not be able to download the package from the internet.");
+      this.output = '';
+      if (navigator.onLine) {
+        setupPyodide();
       }
     }
 
-    Scratch.extensions.register(new Python());
+    getInfo() {
+      return {
+        id: 'MistiumPython',
+        name: 'Python',
+        color1: '#b58707',
+        blocks: [
+          {
+            blockType: Scratch.BlockType.LABEL,
+            text: 'Python Extension Must Be Online To Load Pyodide',
+          },
+          {
+            opcode: 'runPyAsync',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Run Python Async [CODE]',
+            arguments: {
+              CODE: { type: Scratch.ArgumentType.STRING, defaultValue: '' }
+            },
+          },
+          {
+            opcode: 'runPy',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Run Python [CODE]',
+            arguments: {
+              CODE: { type: Scratch.ArgumentType.STRING, defaultValue: '' }
+            },
+          },
+          {
+            opcode: 'evalPyAsync',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Eval Python Async [CODE]',
+            arguments: {
+              CODE: { type: Scratch.ArgumentType.STRING, defaultValue: '' }
+            }
+          },
+          {
+            opcode: 'evalPy',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Eval Python [CODE]',
+            arguments: {
+              CODE: { type: Scratch.ArgumentType.STRING, defaultValue: '' }
+            }
+          },
+          "---",
+          {
+            opcode: 'getvar',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Get Variable [NAME]',
+            arguments: {
+              NAME: { type: Scratch.ArgumentType.STRING, defaultValue: '' }
+            },
+          },
+          {
+            opcode: 'resetvars',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Reset Variables',
+          },
+          "---",
+          {
+            opcode: 'loadPackage',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Load Package [PACKAGE]',
+            arguments: {
+              PACKAGE: { type: Scratch.ArgumentType.STRING, defaultValue: '' }
+            },
+          }
+        ]
+      };
+    }
+
+    async runPyAsync({ CODE }) {
+      try {
+        await this.redirectOutput(async () => await pyodide.runPythonAsync(CODE));
+        return this.output;
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+
+    runPy({ CODE }) {
+      try {
+        this.redirectOutput(() => pyodide.runPython(CODE));
+        return this.output;
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+
+    async evalPyAsync({ CODE }) {
+      try {
+        return await pyodide.runPythonAsync(CODE);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+
+    evalPy({ CODE }) {
+      try {
+        return pyodide.runPython(CODE)
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+    resetvars() {
+      try {
+        pyodide.globals = {};
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+
+    loadPackage({ PACKAGE }) {
+      try {
+        return pyodide.loadPackage(PACKAGE);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+
+    getvar({ NAME }) {
+      try {
+        if (typeof pyodide === 'undefined') {
+          throw new Error("Pyodide object not found.");
+        }
+        return pyodide.globals[NAME];
+      } catch (error) {
+        console.error("Error:", error);
+        return null;
+      }
+    }
+
+    async redirectOutput(func) {
+      try {
+        // Redirect stdout and stderr
+        pyodide.runPython(`
+import sys
+from io import StringIO
+sys.stdout = StringIO()
+sys.stderr = StringIO()
+        `);
+
+        // Run the provided function
+        await func();
+
+        // Get the captured output
+        this.output = pyodide.runPython(`
+output = sys.stdout.getvalue() + sys.stderr.getvalue()
+sys.stdout = sys.__stdout__
+sys.stderr = sys.__stderr__
+output
+        `);
+      } catch (error) {
+        console.error("Error in redirectOutput:", error);
+      }
+    }
+  }
+
+  Scratch.extensions.register(new Python());
 })(Scratch);
