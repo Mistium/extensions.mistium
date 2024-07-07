2eec95d983a22e4e5b77617539ebf31010c90809
Mistium <92952823+Mistium@users.noreply.github.com>
2024-07-07 19:21:06 +0100

    Update IndexedDB.js

--- featured/IndexedDB.js
+++ featured/IndexedDB.js
@@ -6,7 +6,7 @@
 // If a copy of the MPL was not distributed with this file,
 // Then you can obtain one at https://mozilla.org/MPL/2.0/
 
-(function(Scratch) {
+(function (Scratch) {
   "use strict";
 
   class IndexedDB {
@@ -15,6 +15,7 @@
       this.dbName = "scratchDB"; // Default database name
       this.dbVersion = 1;
       this.db;
+      this.initialised = false;
     }
 
     getInfo() {
@@ -23,92 +24,95 @@
         name: 'IndexedDB',
         color1: '#C65B5B',
         blocks: [{
-            opcode: 'setDBName',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'Set database name to [NAME]',
-            arguments: {
-              NAME: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: "scratchDB"
-              }
+          opcode: 'setDBName',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'Set database name to [NAME]',
+          arguments: {
+            NAME: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: "scratchDB"
             }
-          },
-          {
-            opcode: 'writeToDatabase',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'Create Key [KEY] and set it to [VALUE]',
-            arguments: {
-              VALUE: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: ""
-              },
-              KEY: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: "data"
-              }
+          }
+        },
+        {
+          opcode: 'isinitialised',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: 'Is database initialised?',
+        },
+        {
+          opcode: 'writeToDatabase',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'Set Key [KEY] to [VALUE]',
+          arguments: {
+            VALUE: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: ""
+            },
+            KEY: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: "data"
             }
-          },
-          {
-            opcode: 'readFromDatabase',
-            blockType: Scratch.BlockType.REPORTER,
-            text: 'Read value [KEY]',
-            arguments: {
-              KEY: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: "data"
-              }
+          }
+        },
+        {
+          opcode: 'readFromDatabase',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Read value [KEY]',
+          arguments: {
+            KEY: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: "data"
             }
-          },
-          {
-            opcode: 'getAllKeys',
-            blockType: Scratch.BlockType.REPORTER,
-            text: 'Get all keys from database',
-          },
-          {
-            opcode: 'keyExists',
-            blockType: Scratch.BlockType.BOOLEAN,
-            text: 'Key [KEY] exists in database?',
-            arguments: {
-              KEY: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: "data"
-              }
+          }
+        },
+        {
+          opcode: 'getAllKeys',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Get all keys from database',
+        },
+        {
+          opcode: 'keyExists',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: 'Key [KEY] exists in database?',
+          arguments: {
+            KEY: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: "data"
             }
-          },
-          {
-            opcode: 'deleteFromDatabase',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'Delete value with key [KEY] from database',
-            arguments: {
-              KEY: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: "data"
-              }
+          }
+        },
+        {
+          opcode: 'deleteFromDatabase',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'Delete value with key [KEY] from database',
+          arguments: {
+            KEY: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: "data"
             }
-          },
-          {
-            opcode: 'exportDatabaseAsJSON',
-            blockType: Scratch.BlockType.REPORTER,
-            text: 'Export database as JSON',
-          },
-          {
-            opcode: 'importJSONToDatabase',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'Import [jsonData] into database',
-            arguments: {
-              jsonData: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: "{}"
-              }
+          }
+        },
+        {
+          opcode: 'exportDatabaseAsJSON',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Export database as JSON',
+        },
+        {
+          opcode: 'importJSONToDatabase',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'Import [jsonData] into database',
+          arguments: {
+            jsonData: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: "{}"
             }
-          },
+          }
+        },
         ]
       };
     }
 
-    setDBName({
-      NAME
-    }) {
+    setDBName({ NAME }) {
       this.dbName = NAME;
       this.initializeDatabase(); // Re-initialize the database with the new name
     }
@@ -116,13 +120,14 @@
     initializeDatabase() {
       const request = window.indexedDB.open(this.dbName, this.dbVersion);
 
-      request.onerror = function(event) {
+      request.onerror = function (event) {
         console.error("IndexedDB error:", event.target.error);
       };
 
       request.onsuccess = (event) => {
         this.db = event.target.result;
         console.log("IndexedDB initialized successfully!");
+        this.initialised = true;
       };
 
       request.onupgradeneeded = (event) => {
@@ -134,10 +139,11 @@
       };
     }
 
-    writeToDatabase({
-      VALUE,
-      KEY
-    }) {
+    isinitialised() {
+      return this.initialised;
+    }
+
+    writeToDatabase({ VALUE, KEY }) {
       const transaction = this.db.transaction(["data"], "readwrite");
       const objectStore = transaction.objectStore("data");
       objectStore.put({
@@ -146,17 +152,15 @@
       });
     }
 
-    async readFromDatabase({
-      KEY
-    }) {
+    async readFromDatabase({KEY}) {
       return new Promise((resolve, reject) => {
         const transaction = this.db.transaction(["data"], "readonly");
         const objectStore = transaction.objectStore("data");
         const request = objectStore.get(KEY);
-        request.onsuccess = function(event) {
+        request.onsuccess = function (event) {
           resolve(event.target.result ? event.target.result.value : null);
         };
-        request.onerror = function(event) {
+        request.onerror = function (event) {
           reject("Error reading from database");
         };
       });
@@ -167,28 +171,24 @@
         const transaction = this.db.transaction(["data"], "readonly");
         const objectStore = transaction.objectStore("data");
         const request = objectStore.getAllKeys();
-        request.onsuccess = function(event) {
+        request.onsuccess = function (event) {
           const keysArray = event.target.result;
           const keysJSON = JSON.stringify(keysArray); // Convert array to JSON string
           resolve(keysJSON);
         };
-        request.onerror = function(event) {
+        request.onerror = function (event) {
           reject("Error getting keys from database");
         };
       });
     }
 
 
-    async keyExists({
-      KEY
-    }) {
+    async keyExists({ KEY }) {
       const keys = await this.getAllKeys();
       return keys.includes(KEY);
     }
 
-    deleteFromDatabase({
-      KEY
-    }) {
+    deleteFromDatabase({ KEY }) {
       const transaction = this.db.transaction(["data"], "readwrite");
       const objectStore = transaction.objectStore("data");
       objectStore.delete(KEY);
@@ -204,7 +204,7 @@
         const objectStore = transaction.objectStore("data");
         const request = objectStore.getAll();
 
-        request.onsuccess = function(event) {
+        request.onsuccess = function (event) {
           const data = event.target.result;
           try {
             const formattedData = {};
@@ -218,38 +218,38 @@
           }
         };
 
-        request.onerror = function(event) {
+        request.onerror = function (event) {
           reject("Error exporting database as JSON");
         };
       });
     }
-    async importJSONToDatabase({jsonData}) {
-    if (!this.db) {
+    async importJSONToDatabase({ jsonData }) {
+      if (!this.db) {
         return Promise.reject("No database connection available");
-    }
+      }
 
-    return new Promise((resolve, reject) => {
+      return new Promise((resolve, reject) => {
         try {
-            const data = JSON.parse(jsonData);
-            const transaction = this.db.transaction(["data"], "readwrite");
-            const objectStore = transaction.objectStore("data");
+          const data = JSON.parse(jsonData);
+          const transaction = this.db.transaction(["data"], "readwrite");
+          const objectStore = transaction.objectStore("data");
 
-            Object.keys(data).forEach(key => {
-                objectStore.put({ key: key, value: data[key] });
-            });
+          Object.keys(data).forEach(key => {
+            objectStore.put({ key: key, value: data[key] });
+          });
 
-            transaction.oncomplete = function() {
-                resolve("Data imported successfully");
-            };
+          transaction.oncomplete = function () {
+            resolve("Data imported successfully");
+          };
 
-            transaction.onerror = function(event) {
-                reject("Error importing data into database");
-            };
+          transaction.onerror = function (event) {
+            reject("Error importing data into database");
+          };
         } catch (error) {
-            reject("Error parsing JSON data");
+          reject("Error parsing JSON data");
         }
-    });
-}
+      });
+    }
 
   }
   Scratch.extensions.register(new IndexedDB());
