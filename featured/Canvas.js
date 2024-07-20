54ca34b44bc12c9959047f4723981ac91ed6f96d
Mistium <92952823+Mistium@users.noreply.github.com>
2024-07-20 15:54:51 +0100

    Update Canvas.js

--- featured/Canvas.js
+++ featured/Canvas.js
@@ -2,22 +2,6 @@
 // Author: Mistium
 // Description: Create and manipulate canvases with this extension
 
-
-function drawRoundedRect(ctx, x, y, width, height, radius) {
-  ctx.beginPath();
-  ctx.moveTo(x + radius, y);
-  ctx.lineTo(x + width - radius, y);
-  ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-  ctx.lineTo(x + width, y + height - radius);
-  ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-  ctx.lineTo(x + radius, y + height);
-  ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
-  ctx.lineTo(x, y + radius);
-  ctx.quadraticCurveTo(x, y, x + radius, y);
-  ctx.closePath();
-  ctx.stroke();
-}
-
 class CanvasExtension {
   constructor(runtime) {
     this.runtime = runtime;
