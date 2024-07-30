e3269e35d599e935bfff67055f8c919a2ad0a383
Mistium <92952823+Mistium@users.noreply.github.com>
2024-07-30 19:59:07 +0100

    Update KeyHistory.js

--- featured/KeyHistory.js
+++ featured/KeyHistory.js
@@ -1,6 +1,6 @@
 // Name: KeyHistory
 // By: @mistium on discord
-// Description: Store a list of previously pressed keys. Made primarily for use in originOS (https://github.com/Mistium/Origin-OS).
+// Description: Store a list of previously pressed keys and clipboard events.
 // License: MPL-2.0
 // This Source Code is subject to the terms of the Mozilla Public License, v2.0,
 // If a copy of the MPL was not distributed with this file,
@@ -9,21 +9,20 @@
 (function (Scratch) {
   "use strict";
 
-  const MAX_KEY_HISTORY = 100; // Adjust the maximum number of keys to keep in history
-
-  // Define keybinds
-  const keybinds = ["Ctrl", "Shift", "Alt"];
 
   class KeyHistoryExtension {
     constructor() {
       this.keyHistory = [];
       this.pasted = false;
+      this.max_key_history = 100; // Adjust the maximum number of keys to keep in history
+      this.keybinds = ["Ctrl", "Shift", "Alt"]
+      this.pause = false
     }
 
     getInfo() {
       return {
         id: 'keyhistoryextension',
-        name: 'Key History Extension',
+        name: 'Key History',
         color1: '#36644E',
         blocks: [
           {
@@ -51,6 +50,16 @@
                 defaultValue: " "
               },
             },
+          },
+          {
+            opcode: 'enableKeyHistory',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Enable key history',
+          },
+          {
+            opcode: 'disableKeyHistory',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Disable key history',
           },
         ],
       };
@@ -86,7 +95,7 @@
       }
 
       // Add the pressed key to the history
-      if (event.key && event.key.length === 1) {
+      if (event.key && !this.pause) {
         const key = event.key;
         this.addKeyToHistory(key);
       }
@@ -106,17 +115,25 @@
     }
 
     isKeybind(key) {
-      return keybinds.includes(key);
+      return this.keybinds.includes(key);
     }
 
     addKeyToHistory(key) {
       // Check if the maximum history size is reached
-      if (this.keyHistory.length >= MAX_KEY_HISTORY) {
+      if (this.keyHistory.length >= this.max_key_history) {
         this.keyHistory.pop(); // Remove the last element
       }
       
       // Add the key to the end of the array
       this.keyHistory.push(key);
+    }
+
+    enableKeyHistory() {
+      this.pause = false
+    }
+
+    disableKeyHistory() {
+      this.pause = true
     }
   }
 
