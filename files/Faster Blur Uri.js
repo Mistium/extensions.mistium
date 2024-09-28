58c6a63723bdc7f842c9e9f8bd25c7a6f75e87db
Mistium <92952823+Mistium@users.noreply.github.com>
2024-09-28 09:01:25 +0100

    Create Faster Blur Uri.js

---
+++ files/Faster Blur Uri.js
@@ -0,0 +1,46 @@
+class BlurImageExtension {
+  getInfo() {
+    return {
+      id: 'blurImage',
+      name: 'Blur Image',
+      blocks: [
+        {
+          opcode: 'blurImage',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'blur image [URL] by [BLUR] pixels',
+          arguments: {
+            URL: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'https://example.com/image.png'
+            },
+            BLUR: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 5
+            }
+          }
+        }
+      ]
+    };
+  }
+
+  blurImage(args) {
+    const dataUri = args.URL;
+    const blur = args.BLUR;
+    return new Promise((resolve) => {
+      var image = new Image();
+      image.crossOrigin = "anonymous";
+      image.onload = function() {
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext("2d");
+        canvas.width = image.width;
+        canvas.height = image.height;
+        ctx.filter = "blur(" + blur + "px)";
+        ctx.drawImage(image, 0, 0, image.width, image.height);
+        resolve(canvas.toDataURL());
+      };
+      image.src = dataUri;
+    });
+  }
+}
+
+Scratch.extensions.register(new BlurImageExtension());
